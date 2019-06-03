4b36f4e93dad4af5602fb476d282c39f59e0f935
Mason France <masonrf1@gmail.com>
2019-06-03 11:59:57 -0600

    Add Banner component and connect to app

---
+++ src/components/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="banner">
+    <h1>Picsearcht</h1>
+  </header>
+</template>
+
+<script>
+// imports here
+export default {
+  name: 'banner',
+  
+};
+</script>
+
+<style>
+h1 {
+  font-size: 400%;
+  margin-top: 0;
+}
+</style>
